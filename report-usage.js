281a3628b4d4469658d7d3ee82c8038beec96803
BoOpenfin <bo@openfin.co>
2019-07-29 13:19:35 -0400

    use querystring to make full url more readable

--- report-usage.js
+++ report-usage.js
@@ -1,49 +1,23 @@
 'use strict';
 var os = require('os'),
     mid = require('node-machine-id'),
-    https = require('https');
-
-// Get Manifest URL
-var url = [
-    'https://install.openfin.co/installer-usage?',
-    'appName=',
-    'TMP',
-    '&appLocation=',
-    'NULL',
-    '&version=',
-    'undefined',
-    '&machineId=',
-    'NULL',
-    '&manifestUrl=',
-    'NULL',
-    '&platform=',
-    'OSX',
-    '&licenseKey=',
-    'contract_identifier',
-    '&launcherStatus=',
-    'SUCCESS'
-    ],
-    appName = 2,
-    appLocation = 4,
-    version = 6,
-    machineId = 8,
-    manifestUrl = 10,
-    platform = 12,
-    licenseKey = 14,
-    launcherStatus = 16;
+    https = require('https'),
+    querystring = require('querystring');
 
 module.exports = function(status, config, configObj) {
-    var configuredUrl = url.slice(0);
-    configuredUrl[appName] = encodeURIComponent(configObj.startup_app.name || 'TMP');
-    configuredUrl[appLocation] = encodeURIComponent(__dirname);
-    configuredUrl[version] = configObj.runtime.version || 'undefined';
-    configuredUrl[machineId] = mid.machineIdSync({original: true}).toUpperCase();
-    configuredUrl[manifestUrl] = encodeURIComponent(config);
-    configuredUrl[platform] = os.platform().concat('-', os.release());
-    configuredUrl[licenseKey] = configObj.licenseKey || "contract_identifier";
-    configuredUrl[launcherStatus] = status || 'SUCCESS';
+    var reportURL = 'https://install.openfin.co/installer-usage?';
+    var queryObj = {
+        appName: configObj.startup_app.name || 'TMP',
+        appLocation: __dirname,
+        version: configObj.runtime.version || 'undefined',
+        machineId: mid.machineIdSync({original: true}).toUpperCase(),
+        manifestUrl: config,
+        platform: os.platform().concat('-', os.release()),
+        licenseKey: configObj.licenseKey || "contract_identifier",
+        launcherStatus : status || 'SUCCESS'
+    }
 
-    https.get(configuredUrl.join(''), (resp) => {
+    https.get(reportURL.concat(querystring.stringify(queryObj)), (resp) => {
         let data = '';
         resp.on('data', (chunk) => {
             data += chunk;
